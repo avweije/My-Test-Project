356daa32254c23dcadb6d815e41bcb37e47ab5ec
Adriaan van Weije <avweije@adriaans-mbp.fritz.box>
2018-05-27 11:46:02 +0200

    Initial Commit

---
+++ My Test Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Test Project
+//
+//  Created by Adriaan van Weije on 27/05/2018.
+//  Copyright © 2018 Adriaan van Weije. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
